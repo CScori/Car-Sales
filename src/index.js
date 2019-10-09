6a76bd3746c55f0f14148a4008c56dc85f126fcf
CScori <amilanotte1366@gmail.com>
2019-10-09 15:03:41 -0500

    adds prep for connect

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import rootReducer from '../src/reducers'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(rootReducer)
+console.log('store from root', store)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<Provider><App /></Provider>, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
